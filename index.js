db19baa32f0ae8224a3511af44ff6ae1a58fe4d7
ANS <36783010+answebdev@users.noreply.github.com>
2023-09-20 21:56:06 -0700

    add image

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // The Express + Node.js Handbook – Learn the Express JavaScript Framework for Beginners [2022 Edition]:
 // https://www.freecodecamp.org/news/the-express-handbook/
+// Live site: https://express-practice-one.vercel.app/
 
 // To run: nodemon index.js OR node index.js
 const express = require('express');
@@ -7,14 +8,17 @@ const app = express();
 
 // app.set('view engine', 'pug');
 
-// Use the following three lines instead of the previous line: app.set('view engine', 'pug');
+// Use the following 3-4 (4th is optional) lines instead of the previous line: app.set('view engine', 'pug');
 // to delpoy 'pug' template pages on Vercel;
 // otherwise, there will be an Internal Service Error when trying to go to the Pug pages,
 // and the pages will not show.
 // Source: https://stackoverflow.com/questions/76701896/how-should-i-change-the-views-directory-for-vercel-deployment
 app.set("views", __dirname + "/views");
 app.set("view engine", "pug");
+// This line is for the html files in the public folder
 app.use(express.static(__dirname + "public"));
+// This line is for the assets in the public folder (e.g., images)
+app.use(express.static("public"));
 
 // The send() method of the Response object is used to send a simple string as a response.
 // app.get('/', (req, res) => res.send('Hello World!'));
